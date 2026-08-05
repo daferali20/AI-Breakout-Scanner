d8843c442598207cd487ccb6466a416f12115ec1
daferali20 <31751340+daferali20@users.noreply.github.com>
2026-08-06 01:42:45 +0300

    Add files via upload

--- backend/opportunity/__init__.py
+++ backend/opportunity/__init__.py
@@ -1,25 +1,23 @@
-# backend/opportunity/__init__.py
 from .opportunity_engine import OpportunityEngine
-from .models import MarketPhase, OpportunityScoreLevel
+from .models import OpportunityModel
 from .phase_detector import PhaseDetector
 from .transition_model import TransitionModel
 from .catalyst_engine import CatalystEngine
-from .timeline import Timeline
-from .confidence import ConfidenceScore
-from .scoring import ScoreCalculator
+from .timeline import TimelineManager
+from .confidence import ConfidenceCalculator
+from .scoring import ScoringEngine
 from .explanation import ExplanationGenerator
-from .probability import ProbabilityEstimator
+from .probability import ProbabilityEngine
 
 __all__ = [
-    'OpportunityEngine',
-    'MarketPhase',
-    'OpportunityScoreLevel',
-    'PhaseDetector',
-    'TransitionModel',
-    'CatalystEngine',
-    'Timeline',
-    'ConfidenceScore',
-    'ScoreCalculator',
-    'ExplanationGenerator',
-    'ProbabilityEstimator'
+    "OpportunityEngine",
+    "OpportunityModel",
+    "PhaseDetector",
+    "TransitionModel",
+    "CatalystEngine",
+    "TimelineManager",
+    "ConfidenceCalculator",
+    "ScoringEngine",
+    "ExplanationGenerator",
+    "ProbabilityEngine",
 ]
